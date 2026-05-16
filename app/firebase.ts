3baec19687a508655e18e4c18fcd9b4bafcae7e0
design <design@VALUEPLUS.COM>
2026-05-16 12:44:58 +0530

    firebase final fixed

--- app/firebase.ts
+++ app/firebase.ts
@@ -2,17 +2,16 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-
-
 const firebaseConfig = {
-  apiKey: "AIzaSyBTOonTN3bH-jr8hEinlgaZ1QUjXZBJu2w",
-  authDomain: "mychoiceplay-3cc9d.firebaseapp.com",
-  projectId: "mychoiceplay-3cc9d",
-  storageBucket: "mychoiceplay-3cc9d.firebasestorage.app",
-  messagingSenderId: "817877334454",
-  appId: "1:817877334454:web:beeb1941529a02a1f7a917"
+  apiKey: "AIzaSyBEXnPWsZkZ77fStwrdMhIxWSitrJETX3Y",
+  authDomain: "mychoiceplay-ed7ec.firebaseapp.com",
+  projectId: "mychoiceplay-ed7ec",
+  storageBucket: "mychoiceplay-ed7ec.firebasestorage.app",
+  messagingSenderId: "711595930226",
+  appId: "1:711595930226:web:b468843f3590318ced08eb"
 };
 
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
 export const db = getFirestore(app);
