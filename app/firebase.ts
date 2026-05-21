0ab55050254b9a232594363997df2570e39b46b4
design <design@VALUEPLUS.COM>
2026-05-21 11:29:50 +0530

    firebase api fixed

--- app/firebase.ts
+++ app/firebase.ts
@@ -1,18 +1,30 @@
 import { initializeApp } from "firebase/app";
-import { GoogleAuthProvider, getAuth } from "firebase/auth";
+
+import {
+  getAuth,
+  GoogleAuthProvider,
+} from "firebase/auth";
+
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
+  apiKey: "AIzaSyBEXnPWsZkZ77fStwrdMhIxWSitrJETX3Y",
+
   authDomain: "mychoiceplay-ed7ec.firebaseapp.com",
+
   projectId: "mychoiceplay-ed7ec",
+
   storageBucket: "mychoiceplay-ed7ec.firebasestorage.app",
+
   messagingSenderId: "711595930226",
-  appId: "1:711595930226:web:b468843f3590318ced08eb"
+
+  appId: "1:711595930226:web:b468843f3590318ced08eb",
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+
 export const db = getFirestore(app);
+
 export const provider = new GoogleAuthProvider();
